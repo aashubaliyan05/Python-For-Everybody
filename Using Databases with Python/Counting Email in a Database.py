b89f05f01756323b64b127aa39ae6d331340c320
aashubaliyan05 <aashubaliyan05@gmail.com>
2020-08-11 14:54:07 +0530

    week-2-2

---
+++ Using Databases with Python/Counting Email in a Database.py
@@ -0,0 +1,37 @@
+import re
+import sqlite3
+
+connection=sqlite3.connect('emaildb.db')
+cur=connection.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+cur.execute('CREATE TABLE Counts(org TEXT,count INTEGER)')
+
+fname=input("Enter file name: ")
+if (len(fname) < 1): fname = 'mbox.txt'
+fh=open(fname)
+
+
+for line in fh:
+    if not line.startswith('From: '): continue
+    words=line.split()
+    email=words[1].split('@')
+    org=email[1]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,)) #
+
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
+    connection.commit()
+
+
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+
+cur.close()
