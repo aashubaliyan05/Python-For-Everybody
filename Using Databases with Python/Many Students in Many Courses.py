11647209c834b4b157a10e187634197ee502248c
aashubaliyan05 <aashubaliyan05@gmail.com>
2020-08-16 19:24:39 +0530

    week-5

--- Using Databases with Python/Many Students in Many Courses.py
+++ Using Databases with Python/Many Students in Many Courses.py
@@ -74,7 +74,8 @@ for entry in json_data:
 
 
 '''
-    Run:
+    Run the following Query on the database
+    
     SELECT hex(User.name || Course.title || Member.role ) AS X FROM
     User JOIN Member JOIN Course
     ON User.id = Member.user_id AND Member.course_id = Course.id
